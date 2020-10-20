7711d4a64fe301968aa576c16e5e412bf4d726fb
Alessio <alessio.rispoli@ergon.ch>
2020-10-20 16:50:38 +0200

    simp

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo Rostin, wie gahts der so im Lebe?");
+        for (int i = 0; i < 100; i++){
+            System.out.println("Arnel isch en riese Simp");
+        }
     }
 }
